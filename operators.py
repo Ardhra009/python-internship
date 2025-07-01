d3238a2f910f81a3928758a8f71b70ee7e747ce0
Ardhra tb <anaghatb2005@gmail.com>
2025-07-01 09:40:27 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,27 @@
+x=4
+y=9
+print(x//y)
+print(x%y)
+a=3
+a+=3
+print(a)
+print(not(x>3 and x<10))
+k=5
+b=float(k)
+print(b)
+v=["apple","banana"]
+m=["apple","banana"]
+n=v
+print(v is n)
+print(v is not n)
+print("pinapple"in v)
+a="hallo im ardhratb im student"
+print(a.split())
+print(a.replace("hallo","hai"))
+l="     strawberry cake "
+print(l.strip())
+h="     school"
+print(h)
+c="i love applee"
+x=c.count("apple")
+print(x)
